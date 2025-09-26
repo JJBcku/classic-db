eb214c4ad3c1130947d5bb85428db20661610d86
JJBcku <154745800+JJBcku@users.noreply.github.com>
2025-09-26 10:16:09 +0200

    Changed default reputation spillover mask

--- Updates/4769_core_sync.sql
+++ Updates/4769_core_sync.sql
@@ -1,6 +1,6 @@
 ALTER TABLE db_version CHANGE COLUMN required_z2824_01_mangos_model_unification required_z2825_01_mangos_reputation_spillover bit;
 
-ALTER TABLE quest_template ADD COLUMN ReputationSpilloverMask tinyint unsigned NOT NULL DEFAULT '0' AFTER `RewRepValue5`;
+ALTER TABLE quest_template ADD COLUMN ReputationSpilloverMask tinyint unsigned NOT NULL DEFAULT '31' AFTER `RewRepValue5`;
 
 
 
